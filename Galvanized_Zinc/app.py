46a4cd9d18fc4d09f4db120b92623c075abbf069
Rohan_Nandhimandalam <rohan.vjs2004@gmail.com>
2025-08-15 04:25:59 +0530

    Update app.py

--- Galvanized_Zinc/app.py
+++ Galvanized_Zinc/app.py
@@ -76,4 +76,6 @@ def predict():
     return render_template('result.html', predictions=predictions)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render, default 5000 locally
+    app.run(host="0.0.0.0", port=port)
+  
